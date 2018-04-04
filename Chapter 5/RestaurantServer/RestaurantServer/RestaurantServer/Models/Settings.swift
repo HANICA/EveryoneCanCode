9f820a36cddf6f5f9330d6f0d42e2323b6c53824
Johan Korten <johan.korten@han.nl>
2018-04-04 16:17:15 +0200

    Updates
    Updates

---
+++ Chapter 5/RestaurantServer/RestaurantServer/RestaurantServer/Models/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  RestaurantServer
+//
+//  Created by J.A. Korten on 03-04-18.
+//  Copyright © 2018 HAN University of Applied Sciences. All rights reserved.
+//
+
+import Foundation
+
+struct Settings: Codable {
+    var serverPort : UInt16 = 8080
+}
